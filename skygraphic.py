d029985b8997f3e8e39a5d8f008b522c2e4da026
Vybekid <felobitto@gmail.com>
2025-08-04 17:42:49 +0300

    new

---
+++ skygraphic.py
@@ -0,0 +1,20 @@
+from turtle import * 
+import colorsys as cs   
+
+bgcolor("black")
+tracer(5)
+h = 0 
+
+for i in range(250): 
+    c = cs.hsv_to_rgb(h, 1, 1)
+    color(c)
+    begin_fill()
+    forward(i)
+    left(19)
+    backward(i*0.5)
+    circle(i*0.1, 180)
+    h += 0.005 
+    end_fill()
+
+done()
+
